79b39f8765651026625d6d9f9198de58a6c61a1b
MunroRaymaker <jenrud@gmail.com>
2020-05-30 09:22:41 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace BlobQuickstartV12
+{
+    class Program
+    {        
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Azure Blob storage v12 - .NET quickstart sample\n");            
+            string connectionString = "TODO";
+            System.Console.WriteLine("Connection is " + connectionString + "\n");
+
+           // Create a BlobServiceClient object which will be used to create a container client
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            //Create a unique name for the container
+            string containerName = "quickstartblobs" + Guid.NewGuid().ToString();
+
+            // Create the container and return a container client object
+            BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+            
+            // Create a local file in the ./data/ directory for uploading and downloading
+            string localPath = "./data/";
+            string fileName = "quickstart" + Guid.NewGuid().ToString() + ".xml";
+            string localFilePath = Path.Combine(localPath, fileName);
+
+            // Write text to the file
+            await File.WriteAllTextAsync(localFilePath, 
+            @"  <?xml version'1.0' encoding='UTF-8'?>
+                <note>
+                <to>Tove</to>
+                <from>Jani</from>
+                <heading>Reminder</heading>
+                <body>Don't forget me this weekend!</body>
+                </note>");
+
+            // Get a reference to a blob
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+            Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+            // Open the file and upload its data
+            using FileStream uploadFileStream = File.OpenRead(localFilePath);
+            await blobClient.UploadAsync(uploadFileStream, true);
+            uploadFileStream.Close();
+
+            Console.WriteLine("Listing blobs...");
+
+            // List all blobs in the container
+            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+            {
+                Console.WriteLine("\t" + blobItem.Name + "\t" + blobItem.Properties.ContentType + "\t" +
+                blobItem.Properties.ContentLength);
+
+                using MemoryStream ms = new MemoryStream();
+                blobClient.DownloadTo(ms);
+                var foo = Encoding.UTF8.GetString(ms.ToArray());
+                ms.Close();
+
+                // using (var ms = new MemoryStream())
+                // {
+                //     blobClient.DownloadTo(ms);
+                    
+                //     var foo = Encoding.UTF8.GetString(ms.ToArray());   
+                // }
+            }
+
+            // Download the blob to a local file
+            // Append the string "DOWNLOAD" before the .txt extension 
+            // so you can compare the files in the data directory
+            string downloadFilePath = localFilePath.Replace(".xml", "DOWNLOAD.xml");
+
+            Console.WriteLine("\nDownloading blob to\n\t{0}\n", downloadFilePath);
+
+            // Download the blob's contents and save it to a file
+            BlobDownloadInfo download = await blobClient.DownloadAsync();
+
+            using (FileStream downloadFileStream = File.OpenWrite(downloadFilePath))
+            {
+                await download.Content.CopyToAsync(downloadFileStream);
+                downloadFileStream.Close();
+            }
+
+            // Clean up
+            Console.Write("Press any key to begin clean up");
+            //Console.ReadLine();
+
+            Console.WriteLine("Deleting blob container...");
+            await containerClient.DeleteAsync();
+
+            Console.WriteLine("Deleting the local source and downloaded files...");
+            File.Delete(localFilePath);
+            File.Delete(downloadFilePath);
+
+            Console.WriteLine("Done");
+        }
+
+    }
+}
